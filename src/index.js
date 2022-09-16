ae57aca2b175971eca0ddde9a98aa8b802246e95
Vlad Wild <ya.vlash1@yandex.ru>
2022-09-16 17:45:47 +0300

    Parsing nested objects and arrays

--- src/index.js
+++ src/index.js
@@ -1,26 +1,32 @@
 #!/usr/bin/env node
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 const args = process.argv.slice(2);
-let output = 'server.cfg';
+let output = "server.cfg";
 
 if (args.length <= 0) {
-    console.error(new Error(`Failed to specify configuration file. Try 'npx altv-config someFolder/someFile.json'`))
+  console.error(
+    new Error(
+      `Failed to specify configuration file. Try 'npx altv-config someFolder/someFile.json'`
+    )
+  );
 }
 
 if (!fs.existsSync(args[0])) {
-    console.error(new Error(`Could not find file at path: ${args[0]}`))
-    process.exit(1);
+  console.error(new Error(`Could not find file at path: ${args[0]}`));
+  process.exit(1);
 }
 
 // Read output path
 if (args[1]) {
-    if (!args[1].includes('.')) {
-        console.error(new Error(`Output path must have a file name and extension.`))
-        process.exit(1);
-    }
+  if (!args[1].includes(".")) {
+    console.error(
+      new Error(`Output path must have a file name and extension.`)
+    );
+    process.exit(1);
+  }
 
-    output = args[1];
+  output = args[1];
 }
 
 const file = fs.readFileSync(args[0]).toString();
@@ -28,72 +34,72 @@ const file = fs.readFileSync(args[0]).toString();
 let json;
 
 try {
-    json = JSON.parse(file);
+  json = JSON.parse(file);
 } catch (err) {
-    console.error(new Error(`Could not parse JSON file. Is JSON file valid? Try linting online.`))
-    process.exit(1);
+  console.error(
+    new Error(
+      `Could not parse JSON file. Is JSON file valid? Try linting online.`
+    )
+  );
+  process.exit(1);
 }
 
 let dataToWrite = [];
 
-Object.keys(json).forEach(key => {
-    const value = json[key];
-    if (!value) {
+class Parser {
+  constructor(json) {
+    this.parsed = this.parse(json, 0);
+  }
+
+  parse(json, tab) {
+    let data = "";
+    const tabs = (() => "\t".repeat(tab))();
+    Object.keys(json).forEach((key) => {
+      const value = json[key];
+
+      if (value === null || value === undefined) return;
+
+      if (typeof value === "boolean") {
+        data += `${tabs}${key}: ${value}\n`;
         return;
-    }
+      }
 
-    if (typeof value === 'boolean') {
-        dataToWrite.push(`${key}: ${value}`)
+      if (typeof value === "string") {
+        data += `${tabs}${key}: "${value}"\n`;
         return;
-    }
+      }
 
-    if (typeof value === 'string') {
-        dataToWrite.push(`${key}: "${value}"`)
+      if (typeof value === "number") {
+        data += `${tabs}${key}: ${value}\n`;
         return;
-    }
+      }
 
-    if (typeof value === 'number') {
-        dataToWrite.push(`${key}: ${value}`)
+      if (typeof value === "object" && Array.isArray(value)) {
+        data += `${tabs}${key}: [\n`;
+        for (let index = 0; index < value.length; index++) {
+          const element = value[index];
+          data += `${tabs}\t"${element}"\n`;
+        }
+        data += `${tabs}]\n`;
         return;
-    }
+      }
 
-    if (typeof value === 'object' && Array.isArray(value)) {
-        dataToWrite.push(`${key}: ${JSON.stringify(value)}`)
+      if (typeof value === "object") {
+        data += `${tabs}${key}: {\n`;
+        data += `${this.parse(value, tab + 1)}`;
+        data += `${tabs}}\n`;
         return;
-    }
+      }
+    });
 
-    if (typeof value === 'object') {
-        // Should generate similar to:
-        // voice: {
-        //     bitrate: 64000,
-        //     externalSecret: "secret123",
-        // }
-        dataToWrite.push(`${key}: {`)
-        Object.keys(value).forEach(valueKey => {
-            const resultValue = value[valueKey];
-            if (!resultValue) {
-                return;
-            }
+    return data;
+  }
+}
 
-            if (typeof resultValue === 'string') {
-                dataToWrite.push(`  ${valueKey}: "${resultValue}"`)
-                return;
-            }
-
-            if (typeof resultValue === 'number') {
-                dataToWrite.push(`  ${valueKey}: ${resultValue}`)
-                return;
-            }
-        });
-        dataToWrite.push(`}`)
-        return;
-    }
-});
+const parser = new Parser(json);
 
 if (fs.existsSync(output)) {
-    fs.unlinkSync(output);
+  fs.unlinkSync(output);
 }
 
-for (let i = 0; i < dataToWrite.length; i++) {
-    fs.appendFileSync(path.normalize(output), `${dataToWrite[i]}\r\n`);
-}
+fs.appendFileSync(path.normalize(output), `${parser.parsed}`);
